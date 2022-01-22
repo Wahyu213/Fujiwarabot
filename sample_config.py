255b456f6c9712abc692d8b513f5ec19f2200d6e
Wahyusaputra <91831925+Wahyu213@users.noreply.github.com>
2022-01-22 14:08:04 +0700

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from os import environ,path
+if path.exists("config.env"):
+    load_dotenv("config.env")
+
+BOT_TOKEN = environ.get("BOT_TOKEN", None)
+API_ID = int(environ.get("API_ID", 6))
+API_HASH = environ.get("API_HASH", "6875f8920e968bdf69f233182fcf71ee")
+SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
+LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
+GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
+MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
+WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
+MONGO_URL = environ.get("MONGO_URL", None)
+ARQ_API_URL = environ.get("ARQ_API_URL", None)
+ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
+RSS_DELAY = int(environ.get("RSS_DELAY", None))
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Wahyu213/Fujiwarabot.git")
