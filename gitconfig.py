75a5b0ee4fecc0504f16c0f85ec35e679324082f
Wahyusaputra <91831925+Wahyu213@users.noreply.github.com>
2022-01-22 14:11:46 +0700

    Create gitconfig.py

---
+++ gitconfig.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+UPSTREAM_REPO = os.environ.get('UPSTREAM_REPO', "https://github.com/Wahyu213/Fujiwarabot.git")
+try:
+    if len(UPSTREAM_REPO) == 0:
+       raise TypeError
+except TypeError:
+    UPSTREAM_REPO = None
+
+if UPSTREAM_REPO is not None:
+    if os.path.exists('.git'):
+        subprocess.run(["rm", "-rf", ".git"])
+
+    subprocess.run([f"git init -q \
+                      && git config --global user.email chika@kaguyasama.corp \
+                      && git config --global user.name chika \
+                      && git add . \
+                      && git commit -sm update -q \
+                      && git remote add origin {UPSTREAM_REPO} \
+                      && git fetch origin -q \
+                      && git reset --hard origin/master -q"], shell=True)
